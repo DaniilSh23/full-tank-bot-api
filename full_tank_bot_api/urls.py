5c034541473c6af11e2df41b4fba55e7fdaabd73
DaniilSh23 <danyasevas11@gmail.com>
2022-06-19 15:35:59 +0300

    config_for_heroku

--- full_tank_bot_api/urls.py
+++ full_tank_bot_api/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
     path('api/', include('bot_api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
